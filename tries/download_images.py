bd21bf9934b1092c31a4b56cf2a59776da26d99a
Dmitry S Kravchenko <dims12@mail.ru>
2017-05-24 01:13:35 +0300

    some upgrade 2

--- tries/download_images.py
+++ tries/download_images.py
@@ -1,13 +1,23 @@
 import boto3
 
+# s3 = boto3.resource('s3', region_name='us-east-2', aws_access_key_id='AKIAIJDWSJYWKQDPQPFQ',
+#          aws_secret_access_key='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')
+
 s3 = boto3.resource('s3', region_name='us-east-2')
+
+
 bucket = s3.Bucket('sentinel-s2-l1c')
 path = 'tiles/36/R/UU/2017/5/14/0/'
 
+print('Loading B02.jp2')
 object = bucket.Object(path + 'B02.jp2')
 object.download_file('B02.jp2')
+
+print('Loading B03.jp2')
 object = bucket.Object(path + 'B03.jp2')
 object.download_file('B03.jp2')
+
+print('Loading B04.jp2')
 object = bucket.Object(path + 'B04.jp2')
 object.download_file('B04.jp2')
 
